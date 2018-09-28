18e92205a831f39298e96e4f5ad32e6dec482466
albert831229 <albertyang1994@gmail.com>
2018-09-28 13:00:53 -0700

    Upload more json data and a benchmark

---
+++ unmarshal_nested.py
@@ -0,0 +1,19 @@
+import yaml, json, cProfile
+from bravado_core.spec import Spec
+from bravado_core.unmarshal import unmarshal_schema_object
+
+
+def unmarshal_test(data):
+    with open('./spec_nested.yaml', 'r') as f:
+        raw_spec = yaml.load(f)
+    spec = Spec.from_dict(raw_spec)
+    school = raw_spec['definitions']['School']
+
+    school_obj = unmarshal_schema_object(spec, school,data)
+    #print(school_obj)
+
+if __name__ == "__main__":
+    with open('./jsondata/nested10k.txt') as f:
+        data = json.load(f)
+    #unmarshal_test(data)
+    cProfile.run('unmarshal_test(data)', sort='tottime')
