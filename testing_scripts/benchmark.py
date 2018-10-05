cf5be8557506f306f979abb7a3bc4d699c845ba0
PeilinZhong <31148013+PeilinZhong@users.noreply.github.com>
2018-10-05 16:26:03 -0700

    update

--- testing_scripts/benchmark.py
+++ testing_scripts/benchmark.py
@@ -9,6 +9,7 @@ from bravado_core.unmarshal import unmarshal_schema_object
 from unmarshal_array import benchmark_array
 from unmarshal_benchmark import benchmark
 from unmarshal_nested import benchmark_nested
+from unmarshal_inherited import benchmark_inherited
 
 if __name__ == "__main__":
     setup_dict = []
