fb39392c1233680bb45ae9bc8c30476a4e962fa4
albert831229 <albertyang1994@gmail.com>
2018-10-05 10:52:33 -0700

    Add timeit function to benchmark.py

--- testing_scripts/benchmark.py
+++ testing_scripts/benchmark.py
@@ -3,6 +3,7 @@
 import cProfile
 import yaml
 import json
+import timeit
 from bravado_core.spec import Spec
 from bravado_core.unmarshal import unmarshal_schema_object
 from unmarshal_array import benchmark_array
@@ -10,8 +11,13 @@ from unmarshal_benchmark import benchmark
 from unmarshal_nested import benchmark_nested
 
 if __name__ == "__main__":
-    bench = benchmark_array()
-    bench.benchmark()
+    set_up = """
+from unmarshal_array import benchmark_array
+bench = benchmark_array()"""
+
+    func = 'bench.benchmark()'
+    times = timeit.timeit(stmt=func, setup=set_up, number=10)
+    print('benchmark_array(): {}'.format(times))
     #bench = benchmark()
     #bench.unmarshal__10k()
     #bench = benchmark_nested()
--- testing_scripts/unmarshal_array.py
+++ testing_scripts/unmarshal_array.py
@@ -7,17 +7,17 @@ from bravado_core.unmarshal import unmarshal_schema_object
 
 
 class benchmark_array:
-        
+
 
     def __init__(self):
         with open('../specs/spec_array.yaml', 'r') as f:
             self.raw_spec = yaml.load(f)
-        
+
         self.data = self.create_json()
         self.spec = Spec.from_dict(self.raw_spec)
         self.persons = self.raw_spec['definitions']['Persons']
 
-       
+
 
     def create_json(self):
         with open('../jsondata/array10k.txt') as f:
@@ -27,7 +27,3 @@ class benchmark_array:
     def benchmark(self):
         persons_obj = unmarshal_schema_object(self.spec, self.persons,self.data)
         #print (persons_obj)
- 
-
-
-
