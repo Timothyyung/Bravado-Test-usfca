db0d7efb46215f0968e7986d44f893d1298ef2f2
scott <tyung@dons.usfca.edu>
2018-10-12 14:06:05 -0400

    added more data to testing suite

--- testing_scripts/benchmark.py
+++ testing_scripts/benchmark.py
@@ -7,28 +7,45 @@ import threading
 import time
 import json
 import timeit
+import csv
 from unmarshal import benchmark
 
 
 class testingThread(threading.Thread):
     
-    def __init__(self,path1,path2):
+    def __init__(self,path1,path2,testid,bravado_version):
         threading.Thread.__init__(self)
         self.path1 = path1
         self.path2 = path2
+        self.testid = testid
+        self.bench = benchmark(self.path1,self.path2)
+        self.bravado_version = bravado_version
+        self.testsdata = []
 
     def run(self):
-        print("starting testing thread")
-        self.test_benchmark()
+        for i in range(0,10):
+            self.test_benchmark()
+        self.create_csv()
 
 
     def test_benchmark(self):
-        bench = benchmark(self.path1,self.path2)
-        times = timeit.timeit(bench.unmarshal, number = 10)
-        with open('./test.txt', 'a') as outfile:
-            outfile.write('benchmark:' + self.path1 +':{}'.format(times))
+        self.times = timeit.timeit(self.bench.unmarshal, number = 10)
+        res = {'spec'  : self.path1,
+                'times': self.times,
+                'json_size' : self.bench.json_size(),
+                'bravado_version': self.bravado_version}
+        self.testsdata.append(res)
+        print('benchmark:' + self.path1 +':{}'.format(self.times))        
+    
+    def create_csv(self):
+        myFields = ['spec','times','json_size','bravado_version']
 
-        print('benchmark:' + self.path1 +':{}'.format(times))        
+        myFile = open('test'+str(self.testid)+'.csv', 'w')
+        with myFile:
+            writer = csv.DictWriter(myFile, fieldnames=myFields)
+            writer.writeheader()
+            for tdata in self.testsdata:
+                writer.writerow(tdata)
 
 
 
@@ -40,7 +57,7 @@ if __name__ == "__main__":
     
     
     for i in range(0,len(paths),2):
-        thread = testingThread(paths[i], paths[i+1])
+        thread = testingThread(paths[i], paths[i+1],i,"master")
         threadpool.append(thread)
 
     for thread in threadpool:
--- testing_scripts/unmarshal.py
+++ testing_scripts/unmarshal.py
@@ -12,9 +12,9 @@ class benchmark:
             self.raw_spec = yaml.load(f)
 
         self.data = self.create_json(jsonpath)
-        key = list(self.data.keys())
+        self.key = list(self.data.keys())
         self.spec = Spec.from_dict(self.raw_spec)
-        self.obj = self.raw_spec['definitions'][key[0]]
+        self.obj = self.raw_spec['definitions'][self.key[0]]
 
 
 
@@ -26,4 +26,7 @@ class benchmark:
 
     def unmarshal(self):
         obj = unmarshal_schema_object(self.spec, self.obj,self.data)
-        print (obj)
+        #print (obj)
+
+    def json_size(self):
+        return len(self.data[self.key[0]])
